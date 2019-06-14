e96bf0c785dfbe58f7a837bcb5e924d9f6e7319f
ikarostech <ikarostech@live.jp>
2019-06-15 05:28:46 +0900

    pip setup(2回目)

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+	name = 'ObservationHill',
+	version = '0.1.0',
+	url = 'https://github.com/ikarostech/ObservationHill.git',
+	license = 'Free',
+	author = 'ikarostech',
+	author_email = 'ikarostech@live.jp',
+	description = 'From twitter activities, detect suspitious index',
+	install_requires = ['setuptools' , 'scipy' , 'dotenv'],
+	packages = ["topDirectory"],
+	entry_points = {
+		'console_scripts': [
+			'ObservationHill = topDirectory.main:main'
+		]
+	}
+)
