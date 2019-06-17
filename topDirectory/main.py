b4977e36d67fba8276a8fc47b1ef89f4915d2ec3
ikarostech <ikarostech@live.jp>
2019-06-17 23:39:18 +0900

    beta 0.1.0

--- topDirectory/main.py
+++ topDirectory/main.py
@@ -4,7 +4,7 @@ import json
 import statistics
 from scipy import stats
 from os.path import join, dirname
-from dotenv import load_dotenv
+from dotenv import Dotenv
  
 CONSUMER_KEY = ''
 CONSUMER_SECRET = ''
@@ -12,48 +12,50 @@ CONSUMER_SECRET = ''
 def setting():
 	global CONSUMER_KEY
 	global CONSUMER_SECRET
-	dotenv_path = join(dirname(__file__),'.env')
-	load_dotenv(dotenv_path)
+	dotenv = Dotenv(join(dirname(__file__),'.env'))
+	os.environ.update(dotenv)
 
 	CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
 	CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
+
 def verify(auth):
-	
 	redirect_url = auth.get_authorization_url()
 	print('Please verify at: ' + redirect_url)
 	
 	pin = input('input PIN code:')
 	auth.get_access_token(pin)
 
+	
+	api = tweepy.API(auth)
+	return api
+
 def suspicious_index(user_detail):
 	return user_detail.followers_count ** 2 / max((user_detail.statuses_count * user_detail.friends_count),1)
 
-def analyze_user(api,user):
-	user_detail = api.get_user(screen_name=user)
-	followers_detail = api.followers(screen_name=user)
+def analyze(api,screen_name):
+	user_detail = api.get_user(screen_name=screen_name)
+	followers_detail = api.followers(screen_name=screen_name)
 			
 	user_suspicious_index = suspicious_index(user_detail)
 	followers_suspicious_index = stats.hmean([suspicious_index(s) for s in followers_detail])
 
 
 	
-	print("user suspicious index: " + str(user_suspicious_index) )
+	print("user suspicious_index: " + str(user_suspicious_index) )
 	print("followers suspicious index: " + str(followers_suspicious_index) )
 	print("delta index ratio: " + str(user_suspicious_index/followers_suspicious_index) )
-
 def main():
 	setting()
 	
 	auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
-	verify(auth)
-	api = tweepy.API(auth)
+	api = verify(auth)
 
 	while True:
-		print('input user\'s screen name ( @(.*?) ) or input \"quit\": ')
-		user = input()
+		user = input('input user\'s screen name ( @(.*?) ) or input \"quit\" : ')
 		if user == 'quit':
 			break
-		analyze_user(api,user)
+		analyze(api,user)
+	
 
 if __name__ == '__main__':
 	main()
